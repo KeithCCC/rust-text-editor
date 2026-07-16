4e9296ca94aa6ba2add345a856a951aeae9cd3f2
keithCCC <keithc2chen@gmail.com>
2026-07-17 08:59:02 +0900

    chore: update Koharu build timestamp

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
   version: "0.1.3",
-  buildNumber: "20260717.000114",
-  updatedAt: "2026-07-17 00:01:14",
+  buildNumber: "20260717.085802",
+  updatedAt: "2026-07-17 08:58:02",
 } as const;
