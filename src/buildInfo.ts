9bf3f004059f703efcebfb4b7928af71a766456e
keithCCC <keithc2chen@gmail.com>
2026-07-17 00:02:22 +0900

    chore: update Koharu build stamp

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
   version: "0.1.3",
-  buildNumber: "20260716.234245",
-  updatedAt: "2026-07-16 23:42:45",
+  buildNumber: "20260717.000114",
+  updatedAt: "2026-07-17 00:01:14",
 } as const;
